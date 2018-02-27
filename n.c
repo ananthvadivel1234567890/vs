38c3a72eb3ccb4b30332cd4cf5116ed2c856b915
ananthvadivel1234567890 <35652942+ananthvadivel1234567890@users.noreply.github.com>
2018-02-27 16:21:08 +0530

    Create n.c

---
+++ n.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,max;
+    int arr[100];
+ 
+ scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        
+    }
+    max= arr[0];
+    for(i=0;i<n;i++)
+    {
+    if (max<arr[i])
+    {
+    max=arr[i];
+    }
+}
+printf("Maximum element is %d",max);
+}
